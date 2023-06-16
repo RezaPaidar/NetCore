de32fe93b4fa8eebdb3e81e3a591226e8e3c1209
Reza paidar <reza.paidar@outlook.com>
2023-06-16 18:15:56 +0330

    Add files via upload

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services;
+
+namespace CRUDExample.Controllers
+{
+    [Route("persons")]
+    public class PersonsController : Controller
+    {
+        private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
+
+        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
+        {
+            _personsService = personsService;
+            _countriesService =
+            _countriesService = countriesService;
+        }
+
+        [Route("index")]
+        [Route("/")]
+        public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
+        {
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                {nameof(PersonResponse.PersonName),"Person Name"},
+                {nameof(PersonResponse.Email),"Email"},
+                {nameof(PersonResponse.DateOfBirth),"DateOfBirth"},
+                {nameof(PersonResponse.Gender),"Gender"},
+                {nameof(PersonResponse.Country),"Country"},
+                {nameof(PersonResponse.Address),"Address"},
+            };
+
+            List<PersonResponse> persons = _personsService.GetFilterPersons(searchBy, searchString);
+
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
+
+            List<PersonResponse> sortedPersons = _personsService.GetSortedPersons(persons, sortBy, sortOrder);
+            ViewBag.CurrentSortBy = sortBy;
+            ViewBag.CurrentSortOrder = sortOrder.ToString();
+
+            return View(sortedPersons);
+        }
+
+        [Route("create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<CountryResponse> countries = _countriesService.GetAllCountries();
+            ViewBag.Countries = countries;
+            return View();
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public IActionResult Create(PersonAddRequest personAddRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<CountryResponse> countries = _countriesService.GetAllCountries();
+                ViewBag.Countries = countries;
+
+                ViewBag.Errors = ModelState.Values.SelectMany(x => x.Errors).Select(y => y.ErrorMessage).ToList();
+                return View();
+            }
+
+            PersonResponse personResponse = _personsService.AddPerson(personAddRequest);
+            Guid NewPerson = personResponse.PersonID;
+
+            return RedirectToAction("Index", "PersonsController");
+        }
+    }
+}
